2901867f2d99e2955725c0de057ed0cdc34d8398
Ignacio <icarbajop@gmail.com>
2023-07-12 12:48:05 +0800

    Merge branch 'develop' of https://github.com/donavon/use-persisted-state into develop

--- src/createStorage.js
+++ src/createStorage.js
@@ -1,21 +1,12 @@
-const createStorage = provider => ({
+const createStorage = (provider) => ({
   get(key, defaultValue) {
     const json = provider.getItem(key);
-
-    const defaultVal = typeof defaultValue === 'function'
-      ? defaultValue()
-      : defaultValue;
-
-    try {
-      // eslint-disable-next-line no-nested-ternary
-      return json === null || typeof json === 'undefined'
-        ? defaultVal
-        : JSON.parse(json);
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error(err);
-      return defaultVal;
-    }
+    // eslint-disable-next-line no-nested-ternary
+    return json === null || typeof json === 'undefined'
+      ? typeof defaultValue === 'function'
+        ? defaultValue()
+        : defaultValue
+      : JSON.parse(json);
   },
   set(key, value) {
     provider.setItem(key, JSON.stringify(value));

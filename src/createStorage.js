5b80e6235fd82db6ea9ee4de2ce170ac10f158b0
Ignacio <icarbajop@gmail.com>
2023-07-12 12:57:50 +0800

    Keep the try / catch wrapper

--- src/createStorage.js
+++ src/createStorage.js
@@ -1,12 +1,21 @@
-const createStorage = (provider) => ({
+const createStorage = provider => ({
   get(key, defaultValue) {
     const json = provider.getItem(key);
-    // eslint-disable-next-line no-nested-ternary
-    return json === null || typeof json === 'undefined'
-      ? typeof defaultValue === 'function'
-        ? defaultValue()
-        : defaultValue
-      : JSON.parse(json);
+
+    const defaultVal = typeof defaultValue === 'function'
+      ? defaultValue()
+      : defaultValue;
+
+    try {
+      // eslint-disable-next-line no-nested-ternary
+      return json === null || typeof json === 'undefined'
+        ? defaultVal
+        : JSON.parse(json);
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      return defaultVal;
+    }
   },
   set(key, value) {
     provider.setItem(key, JSON.stringify(value));
